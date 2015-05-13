028d36e02853285389a451f145cf313e15386847
Zach Chaney <zachary.chaney@gmail.com>
2015-05-12 20:22:59 -0500

    finished the js game

--- dist/game.js
+++ dist/game.js
@@ -4,25 +4,26 @@ function* chooseYourAdventure() {
 // 	console.log(yield adventure.ask('Woah', 'This is cool'));
 // 	console.log(yield adventure.choose('Woah', 'This is cool', 'Hello world'));
 // 	console.log(yield adventure.say('That\'s all folks!', 'THE END'));
-// 
 
-	yield adventure.say("Welcome, to your adventure");
+	yield adventure.say("Welcome to your adventure, padawan!");
 
 	var myName = yield adventure.ask("What is your name?");
 
-	yield adventure.say("Welcome to the adventure, "+myName);
+	yield adventure.say("Let the adventure begin, "+myName);
 
 	var myWeapon = yield adventure.choose('what weapon would you like to use during your adventure?','Lightsabre','Blaster','Love');
 
 	yield adventure.say('You chose a '+ myWeapon);
 
-	var location = yield adventure.choose('Where would you like to go?, ');
+	var location = yield adventure.choose('Where would you like to go?', 'Tattoine', 'Hoth');
 
 	if(location === 'Tattoine') {
-		yield adventure.say('You die of dehydration');
+		yield adventure.say('You die of dehydration!');
 	}	
 
-	else(location === 'Hoth') {
-		yield adventure.say('You froze to death');
+	else {
+		yield adventure.say('You freeze to death!')
 	}
+
+	yield adventure.say("You died! May the Force be with you!")
 }
